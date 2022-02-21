0ea0d5a54997410983fa575752da1e2284caf3d1
Edder Luan <eluann100@gmail.com>
2022-02-21 01:43:41 -0300

    Página de produtos e página de produto(por id)

--- src/main/java/com/eluanps/mystore/controller/ProdutoController.java
+++ src/main/java/com/eluanps/mystore/controller/ProdutoController.java
@@ -3,12 +3,15 @@ package com.eluanps.mystore.controller;
 import com.eluanps.mystore.entity.Produto;
 import com.eluanps.mystore.service.ProdutoService;
 import java.util.List;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class ProdutoController {
@@ -30,6 +33,21 @@ public class ProdutoController {
         Produto produtos = produtoService.findById(id);
         mv.addObject("produtos", produtos);
         return mv;
+    }
+
+    @RequestMapping(value = "/novoproduto", method = RequestMethod.GET)
+    public String getProdutoForm() {
+        return "novoproduto";
+    }
+
+    @RequestMapping(value = "/novoproduto", method = RequestMethod.POST)
+    public String savePoduto(@Valid Produto produtos, BindingResult result, RedirectAttributes attributes) {
+        if (result.hasErrors()) {
+            attributes.addFlashAttribute("mensagem", "Verifique se os campos obrigatórios foram preenchidos!");
+            return "redirect:/novoproduto";
+        }
+        produtoService.save(produtos);
+        return "redirect:/produtos";
     }
 
 }
