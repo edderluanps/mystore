02a2c35eb969227f2dbe5fd420d142ad5c318911
Edder Luan <eluann100@gmail.com>
2022-02-21 00:55:45 -0300

    Página individual de produtos (FindById)

--- src/main/java/com/eluanps/mystore/controller/ProdutoController.java
+++ src/main/java/com/eluanps/mystore/controller/ProdutoController.java
@@ -5,6 +5,7 @@ import com.eluanps.mystore.service.ProdutoService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,9 +17,17 @@ public class ProdutoController {
     ProdutoService produtoService;
 
     @RequestMapping(value = "/produtos", method = RequestMethod.GET)
-    public ModelAndView getPosts() {
+    public ModelAndView getProdutos() {
         ModelAndView mv = new ModelAndView("produtos");
         List<Produto> produtos = produtoService.findAll();
+        mv.addObject("produtos", produtos);
+        return mv;
+    }
+
+    @RequestMapping(value = "/produtos/{id}", method = RequestMethod.GET)
+    public ModelAndView getProdutosPage(@PathVariable("id") long id) {
+        ModelAndView mv = new ModelAndView("produtopage");
+        Produto produtos = produtoService.findById(id);
         mv.addObject("produtos", produtos);
         return mv;
     }
