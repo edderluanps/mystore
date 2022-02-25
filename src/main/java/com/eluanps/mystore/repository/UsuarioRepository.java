8b9c53b9393833f229f845d8a914db8c7da7d923
Edder Luan <eluann100@gmail.com>
2022-02-25 00:06:11 -0300

    Spring Security config e correção de bugs

--- src/main/java/com/eluanps/mystore/repository/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.eluanps.mystore.repository;
-
-import com.eluanps.mystore.entity.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-    
-}
